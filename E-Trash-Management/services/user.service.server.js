8ba0d9f28b859b841f609465126d43ff318f842e
tarun <singh.tar@husky.Neu.edu>
2017-12-05 18:20:04 -0500

    added user service, model, schema, server.js change,user service.client
    added user service, model, schema, server.js change,user service.client

---
+++ E-Trash-Management/services/user.service.server.js
@@ -0,0 +1,106 @@
+
+
+module.exports = function(app) {
+  var userModel = require("../model/user/user.model.server");
+
+  app.post('/api/user' , createUser);
+  app.get('/api/user', findUser);
+  app.get('/api/user/:userId',findUserById );
+  app.put('/api/user/:userId', updateUser);
+  app.delete('api/user/:userId', deleteUser);
+
+  function findUserById(req, res) {
+    var userId = req.params["userId"];
+    userModel.findUserById(userId).then(function (user) {
+      if (user) {
+        res.json(user);
+
+      }
+      else {
+        res.json({});
+      }
+    });
+  }
+
+  function findUser(req, res){
+    var username = req.query['username'];
+    var password = req.query['password'];
+    if(username && password) {
+      var promise = userModel.finduserByCredentails(username, password);
+      promise.then(function(user){
+        res.json(user);
+        console.log(user);
+      });
+      return;
+    } else if(username) {
+      userModel
+        .findUserByUsername(username)
+        .then(function(user){
+          res.json(user);
+          console.log(user);
+        });
+      return;
+    } else {
+      res.status(404).send({error: "Not found"});
+    }
+  }
+
+
+  function finduserByCredentails(username,password) {
+    var promise = userModel.finduserByCredentails(username, password);
+    promise.then(function(user){
+      res.json(user);
+      console.log(user);
+    });
+  }
+
+
+  function findUserById(req,res){
+    var userId = req.params["userId"];
+    var promise = userModel.findUserById(userId);
+    promise.then(function (result) {
+      if(result){
+        res.json(result);
+      }
+      else {
+        res.status(404).send({ error: "User not Found" });
+      }
+    });
+    return;
+  }
+
+
+
+  function createUser(req, res) {
+    var newUser = req.body;
+    userModel.createUser(user)
+      .then(function (user) {
+        res.json(user);
+      });
+  }
+
+
+
+  function updateUser(req, res) {
+    var uid = req.params["userId"];
+    var newUser = req.body;
+    userModel.updateUser(uid,newUser)
+      .then(function (status) {
+        console.log(status);
+        res.send(status);
+      }, function (err) {
+        console.log(err);
+      });
+    return;
+  }
+
+  function deleteUser(req, res) {
+    var uid = req.params["userId"];
+    userModel
+      .deleteUser(uid)
+      .then(function (user) {
+        res.json(user);
+      });
+  }
+
+};
---
+++ E-Trash-Management/src/app/services/user.service.client.ts
@@ -0,0 +1,78 @@
+import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/Rx';
+import { environment } from '../../environments/environment';
+
+@Injectable()
+export class Userservice {
+
+
+  baseUrl = environment.baseUrl;
+
+  constructor(private http: Http) {
+  }
+
+
+  finduserByCredentials(username: String, password: String): Observable <any> {
+    console.log('I am in client');
+    const url =  this.baseUrl + '/api/user?username=' + username + '&password=' + password;
+    return this.http.get(url)
+      .map(
+        (response: Response) => {
+          return response.json();
+        });
+  }
+
+  createUser(user: any): Observable <any> {
+    const url = this.baseUrl + '/api/user';
+    return this.http.post(url, user)
+      .map(
+        (response: Response) => {
+          return response.json();
+        }
+      );
+  }
+
+  findUserById(userid: String): Observable <any> {
+    const url = this.baseUrl + '/api/user/' + userid;
+    return this.http.get(url)
+      .map(
+        (response: Response) => {
+          return response.json();
+        }
+      );
+
+  }
+
+  findUserByUsername(username: String): Observable <any> {
+    const url = this.baseUrl + '/api/user?username=' + username;
+    return this.http.get(url)
+      .map(
+        (response: Response) => {
+          return response.json();
+        });
+  }
+
+
+
+  updateUser(userId, user): Observable <any> {
+    const url = this.baseUrl + '/api/user/' + userId;
+    return this.http.put(url, user)
+      .map(
+        (response: Response) => {
+          return response.json();
+        }
+      );
+  }
+
+  deleteUser(userId: string): Observable <any> {
+    const url = this.baseUrl + '/api/user' + userId;
+    return this.http.delete(url)
+      .map(
+        (response: Response) => {
+          return response.json();
+        }
+      );
+  }
+}
