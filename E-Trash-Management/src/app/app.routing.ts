a01276d0ad09fa5470e222c651e2d3d3258349a0
hemanth.shetty.m@gmail.com <hemanth.shetty.m@gmail.com>
2017-12-05 17:49:15 -0500

    Register routing
    Register routing

--- E-Trash-Management/src/app/app.routing.ts
+++ E-Trash-Management/src/app/app.routing.ts
@@ -5,9 +5,12 @@
 import {Routes, RouterModule} from '@angular/router';
 import {HomeComponent} from './components/home/home.component';
 import {ModuleWithProviders} from '@angular/core';
+import {RegisterComponent} from "./components/user/register/register.component";
 
 const APP_ROUTES: Routes = [
-  {path: '', component : HomeComponent}
+  {path: '', component : HomeComponent},
+  {path: 'home', component : HomeComponent},
+  {path: 'register', component : RegisterComponent}
 ];
 
 // Export the routes as module providers
