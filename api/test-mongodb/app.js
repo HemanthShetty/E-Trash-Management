50c37f4289aa5427bfd0c345845061a831c39b7f
hemanth.shetty.m@gmail.com <hemanth.shetty.m@gmail.com>
2017-12-08 16:05:56 -0500

    fix
    fix

--- api/test-mongodb/app.js
+++ api/test-mongodb/app.js
@@ -1,12 +1,5 @@
 module.exports = function(app)
 {
-
-
-    app.get("/api/test", findAllMessages);
-    app.post("/api/test", createMessage);
-    app.delete("/api/test/:id", deleteMessage);
-
-
   var connectionString = 'mongodb://127.0.0.1:27017/test'; // for local
 
   if(process.env.MLAB_USERNAME_WEBDEV) { // check if running remotely
