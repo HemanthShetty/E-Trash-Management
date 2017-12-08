ad3976e4513778a9e34b355b7c939e238e15788a
hemanth.shetty.m@gmail.com <hemanth.shetty.m@gmail.com>
2017-12-08 15:40:40 -0500

    Connection debug
    Connection debug

--- api/db/connection.js
+++ api/db/connection.js
@@ -2,6 +2,7 @@ var connectionString = 'mongodb://127.0.0.1:27017/test'; // for local
 if(process.env.MLAB_USERNAME_WEBDEV) { // check if running remotely
   var username = process.env.MLAB_USERNAME_WEBDEV; // get from environment
   var password = process.env.MLAB_PASSWORD_WEBDEV;
+  console.log(username+password);
   connectionString = 'mongodb://' + username + ':' + password;
   connectionString += 'ds133816.mlab.com:33816/heroku_nqzdnx12';
 }
